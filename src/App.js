8023606ae82c778e5e312444d7706166b2729d6d
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-05 00:37:10 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { io } from "socket.io-client";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import SocketContext from './context/SocketContext';
+import AuthContext from './context/AuthContext';
+import { useAuth } from './hooks/AuthHook';
+import Header from './components/Header';
 
 const App = () => {
+  const [socket, setSocket] = useState();
+
+  useEffect(() => {
+    setSocket(io(process.env.REACT_APP_BACKEND_URL));
+  }, []);
+
+  const { token, userId, login, logout } = useAuth();
+
+  useEffect(() => {
+    if (!!token && socket) {
+      socket.emit('user active', { userId: userId });
+    }
+  }, [socket, token]);
+
   return (
-    <React.Fragment>App.js</React.Fragment>
+    <React.Fragment>
+      {socket && (
+        <SocketContext.Provider
+          value={{
+            socket: socket
+          }}>
+          <AuthContext.Provider
+            value={{
+              isLoggedIn: !!token,
+              token: token,
+              userId: userId,
+              login: login,
+              logout: logout
+            }}
+          >
+            <Header />
+            <Routes>
+              {!!token ? (
+                <React.Fragment>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/auth" element={<Auth />} />
+                </React.Fragment>
+              ) : (
+                <Route path="*" element={<Auth />} />
+              )}
+            </Routes>
+          </AuthContext.Provider>
+        </SocketContext.Provider>
+      )}
+    </React.Fragment>
   );
 };
 
---
+++ src/components/ChatContainer.js
@@ -0,0 +1,130 @@
+import React, { useContext, useEffect, useState } from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useHttpClient } from '../hooks/HttpHook';
+import AuthContext from '../context/AuthContext';
+import SocketContext from '../context/SocketContext';
+
+const ChatContainer = ({ selectedChat }) => {
+    const [chatMessages, setChatMessages] = useState();
+    const [messageText, setMessageText] = useState('');
+
+    const { sendRequest } = useHttpClient();
+
+    const auth = useContext(AuthContext);
+
+    const { socket } = useContext(SocketContext);
+
+    useEffect(() => {
+        socket.on("msg-recieve", ({ sender, message }) => {
+            if (selectedChat && sender.toString() === selectedChat.toString()) {
+                setChatMessages(prevState => {
+                    return [
+                        ...prevState,
+                        message
+                    ]
+                })
+            }
+        });
+    }, [selectedChat]);
+
+    useEffect(() => {
+        if (selectedChat) {
+            sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/user/messages/${selectedChat}`,
+                'GET',
+                null,
+                {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + auth.token
+                }
+            )
+                .then(({ messages }) => {
+                    setChatMessages(messages);
+                })
+        }
+    }, [selectedChat]);
+
+    return (
+        <React.Fragment>
+            {selectedChat ? (
+                <React.Fragment>
+                    <Row style={{
+                        height: "90%",
+                        overflow: "scroll"
+                    }} className="d-flex flex-column p-3">
+                        {chatMessages && chatMessages.map((msg, index) => {
+                            return (
+                                <Row
+                                    key={index}
+                                    className={`mx-0 py-2 px-3 my-1 rounded-5 text-white ${msg.fromSelf ? 'bg-primary' : 'bg-secondary'}`}
+                                    style={{
+                                        maxWidth: "40%",
+                                        alignSelf: `${msg.fromSelf ? 'end' : 'start'}`
+                                    }}>
+                                    {msg.text}
+                                </Row>
+                            )
+                        })}
+                    </Row>
+                    <Row>
+                        <Form
+                            onSubmit={event => {
+                                event.preventDefault();
+
+                                sendRequest(`${process.env.REACT_APP_BACKEND_URL}/api/user/send-message`,
+                                    'POST',
+                                    JSON.stringify({
+                                        text: messageText,
+                                        to: selectedChat
+                                    }),
+                                    {
+                                        'Content-Type': 'application/json',
+                                        Authorization: 'Bearer ' + auth.token
+                                    }
+                                ).then(res => {
+                                    socket.emit("send-msg", {
+                                        from: auth.userId,
+                                        to: selectedChat,
+                                        text: messageText
+                                    })
+                                    setChatMessages(prevState => {
+                                        return [
+                                            ...prevState,
+                                            {
+                                                text: messageText,
+                                                fromSelf: true
+                                            }
+                                        ]
+                                    })
+                                    setMessageText('');
+                                })
+                            }}>
+                            <Row>
+                                <Col xs={11} className="d-flex flex-column justify-content-center">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Type your message here..."
+                                        className="py-2 h-100"
+                                        onChange={(e) => setMessageText(e.target.value)}
+                                        value={messageText}
+
+                                    />
+                                </Col>
+                                <Col xs={1}>
+                                    <Button
+                                        type="submit">Send</Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Row>
+                </React.Fragment>
+            ) : (
+                <h1>Please select a user to see messages.</h1>
+            )}
+        </React.Fragment>
+    );
+};
+
+export default ChatContainer;
